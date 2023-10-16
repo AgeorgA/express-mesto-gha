bb89fc89f785421cff11e093703bf1e20e4d78c7
Egor And <georgand@yandex.ru>
2023-10-16 21:11:45 +0300

    S12R1

--- router/index.js
+++ router/index.js
@@ -5,6 +5,7 @@ const cardRoutes = require("./cards");
 
 router.use("/users", userRoutes);
 router.use("/cards", cardRoutes);
+
 // router.use((req, res) => {
 //   res.status(404).send({ message: 'Ошибка - 404 Страница не найдена' });
 // });
